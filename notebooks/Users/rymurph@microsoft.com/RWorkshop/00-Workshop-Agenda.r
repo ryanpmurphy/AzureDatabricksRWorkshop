5b0ec7d12a2921faaabc3f09968b8c3ce5a11e43
Ryan Murphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-02-26 22:24:53 -0600

    Update: README.md notebooks

---
+++ notebooks/Users/rymurph@microsoft.com/RWorkshop/00-Workshop-Agenda.r
@@ -0,0 +1,64 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Azure Databricks Workshop Agenda
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Create Azure Databricks workspace
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Import workshop notebooks
+# MAGIC 
+# MAGIC #####*_NOTES:_*
+# MAGIC * Substitute placeholders with your values (e.g., `{YOUR CONTAINER NAME}`, `{YOUR SQL DATABASE NAME}`)
+# MAGIC * Blob Storage Account and your container is already created
+# MAGIC * logical SQL Server is already created, but you will need to create your own SQL Database
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Create a cluster
+# MAGIC * Include storage config: `spark.hadoop.fs.azure.account.key.adbbase.blob.core.windows.net XwYaFpISJYnw8q7iaYPmAv/hHSlhzThXZVAj6X4j75MlN7/6gYJKq6yjgN7lE4t0YQ9vnnVK+vRYHPYDkWeHww==`
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Explore notebook experience
+# MAGIC * REPL
+# MAGIC * Magics
+# MAGIC * Markdown (cheat sheet: https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)* Collaboration features
+# MAGIC * Git integration
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Create an Azure SQL Database
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Walk through each notebook
+# MAGIC * Connect to Blob Storage
+# MAGIC * Get Started with Data in SparkR
+# MAGIC * Work with Databases and Tables
+# MAGIC * Visualize Your Data
+# MAGIC * Work with Time Series Data
+# MAGIC * Read and Write to Azure SQL Database
+# MAGIC * Machine Learning with R and SparkR
+# MAGIC * Schedule Batch Predictions
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Create a scheduled job
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Continue exploring and experimenting!
+
+# COMMAND ----------
+
